9914979ba416b240a8c8eaeeaa4313f41c5d2396
asus <asus@gmail.com>
2021-12-05 18:00:36 +0630

    'sixpm'

--- photos/views.py
+++ photos/views.py
@@ -20,19 +20,19 @@ def addPhoto(request):
     categories=Category.objects.all()
     if request.method == 'POST':
         data = request.POST
-        image = request.FILES.get('image')
+        image = request.FILES.getlist('image')
         if data['category']!='none':
             category=Category.objects.get(id=data['category'])
         elif data['category']!='':
             category,created = Category.objects.get_or_create(name=data['category_new'])
         else:
             category = None
-
-        photo=Photo.objects.create(
-            category=category,
-            description = data['description'],
-            image=image
-        )
+        for image in image:
+            photo=Photo.objects.create(
+                category=category,
+                description = data['description'],
+                image=image
+            )
         return redirect('gallery')
 
     context = {'categories':categories}
